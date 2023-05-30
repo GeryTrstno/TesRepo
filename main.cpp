b880b8f7e19d5ea0ef0fbb3314778f7f0f4f7557
GeryTrstno <gerytauristino8@gmail.com>
2023-05-30 19:35:12 +0700

    feat: Buat cin cout simple

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
+    cout << "Angka yang anda masukkan adalah " + N << endl;
 
-    cout << "Angka yang anda masukkan adalah " + N;
+    for (int i = 0; i < N; i++) {
+        cout << "Angka : " << i << endl;
+    }
 
     return 0;
 }
